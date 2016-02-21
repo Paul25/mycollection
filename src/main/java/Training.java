e4b2bf36bb6e7e24ed80726fae99e5ca5cffbb72
Paul25 <missile2013@yandex.ru>
2016-02-21 13:47:29 +0200

    21.02-13.40

---
+++ src/main/java/Training.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Marchenko on 21.02.2016.
+ */
+public class Training {
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Hello");
+    }
+}
