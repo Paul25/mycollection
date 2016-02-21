da6697e3a73b7b6865eb79f645885bd809ff43aa
Paul25 <missile2013@yandex.ru>
2016-02-21 13:46:45 +0200

    21.02-13.45

---
+++ src/main/java/Study.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Marchenko on 21.02.2016.
+ */
+public class Study {
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Hello world");
+    }
+}
